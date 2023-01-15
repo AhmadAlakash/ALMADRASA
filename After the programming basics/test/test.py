f46fc046f78d5a248f4135689a7d468d3c4d3c27
ahmala123 <ahmadalakash000@gmail.com>
2023-01-15 16:29:19 +0100

    n

---
+++ After the programming basics/test/test.py
@@ -0,0 +1,27 @@
+
+list_test=["a","b","c","d"]
+
+
+print(list_test[3])
+
+list_test[3]="z"
+print(list_test[3])
+
+
+
+
+phoneBook = {
+    "ahmad":"015774901949", 
+    "Alma":"0166656464662", 
+    "mohamed":"0159985444", 
+    "yousef":"01544856246",
+   
+    "ahmadal":"0154489556", 
+}
+
+# to acsses of key or value from our dict 
+
+print (phoneBook["yousef"])
+
+phoneBook["ahmadal"]="0154455"   # to change 
+print(phoneBook["ahmadal"])
