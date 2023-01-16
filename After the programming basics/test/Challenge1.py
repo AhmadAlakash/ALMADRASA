3c6f1b27c7c5297fbc0406aaec5351c93178171e
ahmala123 <ahmadalakash000@gmail.com>
2023-01-16 22:50:05 +0100

    n

---
+++ After the programming basics/test/Challenge1.py
@@ -0,0 +1,40 @@
+# Nearest stars to Earth 
+
+stars=["Sol","Alpha Centauri","Barnard","Wolf 359",]
+
+print(stars[3])
+#stars.append("3")
+#print(stars)
+
+
+
+# Hight peak on each tectonic plate
+
+African = "Kilimanjaro"
+Antarctic = "Vinson"
+Australian = "Puncak Jaya"
+Eurasian = "Everest"
+North_American = "Denali"
+Pacific = "Mauna kea"
+South_American = "Aconcagua"
+
+
+
+peaks = {
+    "African" : "Kilimanjaro",
+   " Antarctic" : "Vinson",
+   " Australian" : "Puncak Jaya",
+   " Eurasian":"Everest",
+    "North_American" : "Denali",
+    "Pacific" : "Mauna kea",
+    "South_American" : "Aconcagua",
+   
+   
+
+
+}
+
+
+
+print(peaks["Pacific"])
+
---
+++ After the programming basics/test/loops.py
@@ -0,0 +1,14 @@
+animals = [
+    "Tiger",
+    "Lion",
+    "Fox",
+    "Cat",
+    "Dog",
+    "Wolf",
+]
+
+for animal in animals :
+    
+    upercaseString = str(animal).upper()
+    print (upercaseString)
+
